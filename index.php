78f46eff47610c6334747ae7bdf9069ca64d4eac
Victor-Ugwueze <victorugwueze@gmail.com>
2018-08-31 16:38:06 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+use Endroid\QrCode\QrCode;
+
+$qrCode = new QrCode('Life is too short to be generating QR codes');
+
+header('Content-Type: '.$qrCode->getContentType());
+echo $qrCode->writeString();
